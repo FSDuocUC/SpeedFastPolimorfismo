e71f1377073cd63b588324b4329c077a070674de
fabián <fabianalexandermagnum@gmail.com>
2026-08-15 23:37:42 -0400

    Primer commit: Proyecto SpeedFastPolimorfismo

--- src/ui/Main.java
+++ src/ui/Main.java
@@ -13,7 +13,7 @@ public class Main {
         encomienda.asignarRepartidor();
         express.asignarRepartidor();
 
-        // Metodos sobrecargados
+        // Métodos sobrecargados
         comida.asignarRepartidor("Carlos");
         encomienda.asignarRepartidor("Ana");
         express.asignarRepartidor("Luis");
